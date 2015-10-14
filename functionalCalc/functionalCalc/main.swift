7362bb59f66c6b221b29e2e6976971a12f16e86e
Jason Ho <raktos@uw.edu>
2015-10-13 18:47:11 -0700

    otionals work

--- functionalCalc/functionalCalc/main.swift
+++ functionalCalc/functionalCalc/main.swift
@@ -8,71 +8,99 @@
 
 import Foundation
 
-//TODO optionals everywhere
-
 //////////////////////
 //Build a Calculator//
 //////////////////////
 func add(a : Int?, b : Int?) -> Int? {
-    if a == nil || b == nil {
+    if nilCheck([a,b]) {
         return nil
     }
     return a! + b!
 }
 
-func sub(a : Int, b : Int) -> Int {
-    return a - b
-}
-
-func mul(a : Int, b : Int) -> Int {
-    return a * b
-}
-
-func div(a : Int, b : Int) -> Int? {
-    if b != 0 {
-        return a / b
+func sub(a : Int?, b : Int?) -> Int? {
+    if nilCheck([a,b]) {
+        return nil
     }
-    return nil
+    return a! - b!
 }
 
-func mathOp(a : Int, b : Int, op : (Int, Int) -> Int) -> Int? {
+func mul(a : Int?, b : Int?) -> Int? {
+    if nilCheck([a,b]) {
+        return nil
+    }
+    return a! * b!
+}
+
+func div(a : Int?, b : Int?) -> Int? {
+    if nilCheck([a,b]) || b == 0 {
+        return nil
+    }
+    return a! / b!
+}
+
+func mathOp(a : Int?, b : Int?, op : (Int?, Int?) -> Int?) -> Int? {
     return op(a, b)
+}
+
+//nil check
+func nilCheck<T>(objs : [T?]) -> Bool {
+    for obj in objs {
+        if obj == nil {
+            return true
+        }
+    }
+    return false
 }
 
 /////////////
 //Array Fun//
 /////////////
-func arrayAdd(nums : [Int]) -> Int {
+func arrayAdd(nums : [Int]?) -> Int? {
+    if nums == nil {
+        return nil
+    }
     var total = 0
-    for n in nums {
+    for n in nums! {
         total += n
     }
     return total
 }
 
-func arrayMul(nums : [Int]) -> Int {
+func arrayMul(nums : [Int]?) -> Int? {
+    if nums == nil {
+        return nil
+    }
     var total = 0
-    for n in nums {
+    for n in nums! {
         total *= n
     }
     return total
 }
 
-func arrayCount(nums : [Int]) -> Int {
-    return nums.count
+func arrayCount(nums : [Int]?) -> Int? {
+    if nums == nil {
+        return nil
+    }
+    return nums!.count
 }
 
-func arrayAvg(nums : [Int]) -> Int {
+func arrayAvg(nums : [Int]?) -> Int? {
+    if nums == nil {
+        return nil
+    }
     var total = 0
-    for n in nums {
+    for n in nums! {
         total += n
     }
-    return total / nums.count
+    return total / nums!.count
 }
 
-func arrayOp(nums : [Int], op : ([Int]) -> Int) -> Int {
+func arrayOp(nums : [Int]?, op : ([Int]?) -> Int?) -> Int? {
     return op(nums)
 }
+
+
 
 //////////
 //Points//
@@ -159,29 +187,29 @@ func pointSub(var a : [String : Double], var b : [String : Double]) -> (Double, 
 
 
 //Calculator Tests
-print("Calculator Tests\n")
-print("add 3 + 5 = \(add(3,5))\n")
-print("sub 5 - 3 = \(sub(5,3))\n")
-print("mul 6 * 7 = \(mul(6,7))\n")
+print("Calculator Tests:\n")
+print("add 3 + 5 = \(add(3,5)!)\n")
+print("sub 5 - 3 = \(sub(5,3)!)\n")
+print("mul 6 * 7 = \(mul(6,7)!)\n")
 print("div 12 / 3 = \(div(12,3)!)\n")
 print("div 12 / 0 = \(div(12,0))\n")
-print("mathOp add 5 + 10 = \(mathOp(5,10,add))\n")
+print("mathOp add 5 + 10 = \(mathOp(5,10,add)!)\n")
+print("nil add 5 + nil = \(add(5,nil))\n")
 print("\n")
 
 //Array Tests
-print("Array Tests\n")
+print("Array Tests:\n")
 print("add 3 + 4 + 5 + 6 = \(arrayAdd([3,4,5,6]))\n")
 print("count 3, 4, 5, 6 = \(arrayCount([3,4,5,6]))\n")
 print("avg 1, 5, 2, 4 = \(arrayAvg([1,5,2,4]))\n")
 print("arrayOp avg 1, 5, 2, 4 = \(arrayOp([1,5,2,4], arrayAvg))\n")
+print("add nil = \(arrayAdd(nil))\n")
 print("\n")
 
 //Point Tests
-
-print("Point Tests")
+print("Point Tests:")
 print("Tuple add (1, 2) + (3, 4) = \(pointAdd((1,2),(3,4)))\n")
 print("Tuple sub (1, 2) - (3, 4) = \(pointSub((1,2),(3,4)))\n")
-//print("Tuple add (1, 2, 3) + (3, 4) = \(pointAdd(1,2,3,4),(3,4))\n")
 let point1 = ["x":1,"y":2]
 let point2 = ["x":3,"y":4]
 let doublePoint = ["x":1.5,"y":2.0]
